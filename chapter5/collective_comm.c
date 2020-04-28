891f7f5831f67cd2ba320bb7ca7521a23236faf7
Gudmundur Heimisson <gudmundur.heimisson@gmail.com>
2020-04-27 20:46:33 -0700

    Add matrix vector product exercise

--- chapter5/collective_comm.c
+++ chapter5/collective_comm.c
@@ -3,6 +3,27 @@
 #include <math.h>
 #include "mpi.h"
 
+float func(float x) {
+  return powf(x, 2.0);
+}
+
+float func_integral(float left, float right) {
+  return (1.0/3.0) * (powf(right, 3.0) - powf(left, 3.0));
+}
+
+float trapezoid(float left_bound, float right_bound, int num_traps) {
+  float interval_size = (right_bound - left_bound) / (float) num_traps;
+  float right_value = func(right_bound);
+  float left_value = func(left_bound);
+  float total = 0.5 * (left_value + right_value);
+  for (int trap_idx = 1; trap_idx < num_traps; ++trap_idx) {
+    float x = left_bound + trap_idx * interval_size;
+    total += func(x);
+  }
+  total = interval_size * total;
+  return total;
+}
+
 void get_parameters(float* left_bound, float* right_bound, int* num_traps,
                     int comm_rank, int comm_size) {
   int source = 0;
@@ -32,8 +53,22 @@ int main(int argc, char* argv[]) {
   float right_bound;
   int num_traps;
   get_parameters(&left_bound, &right_bound, &num_traps, comm_rank, comm_size);
-  printf("Process %d, left bound: %f, right bound: %f, segments: %d\n",
-         comm_rank, left_bound, right_bound, num_traps);
+  float true_value = func_integral(left_bound, right_bound);
+
+  float interval_size = (right_bound - left_bound) / comm_size;
+  float local_left_bound = left_bound + comm_rank * interval_size;
+  float local_right_bound = local_left_bound + interval_size;
+  float local_total = trapezoid(local_left_bound, local_right_bound,
+                                num_traps);
+
+  float total;
+  MPI_Reduce(&local_total, &total, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+  if (comm_rank == 0) {
+    printf("True value: %.3f\n", true_value);
+    printf("Calculated value: %.3f\n", total);
+    float relative_error = (total - true_value) / true_value;
+    printf("Relative error: %.3f%%\n", 100.0 * relative_error);
+  }
 
   MPI_Finalize();
   return 0;
---
+++ chapter5/matrix_vector_product.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "mpi.h"
+
+void print_matrix(int num_rows, int num_cols, float matrix[num_rows][num_cols]) {
+  printf("Matrix:\n");
+  for (int row_idx = 0; row_idx < num_rows; ++row_idx) {
+    for (int col_idx = 0; col_idx < num_cols; ++col_idx) {
+      printf("%.1f\t", matrix[row_idx][col_idx]);
+    }
+    printf("\n");
+  }
+}
+
+void print_vector(int size, float vector[size]) {
+  printf("Vector: ");
+  for (int i = 0; i < size; ++i) {
+    printf("%.1f ", vector[i]);
+  }
+  printf("\n");
+}
+
+void serial_product(int num_rows, int num_cols, float matrix[num_rows][num_cols],
+                    float vector[num_cols], float result[num_rows]) {
+  for (int row_idx = 0; row_idx < num_rows; ++row_idx) {
+    result[row_idx] = 0;
+    for (int col_idx = 0; col_idx < num_cols; ++col_idx) {
+      result[row_idx] += vector[col_idx] * matrix[row_idx][col_idx];
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  MPI_Init(&argc, &argv);
+  int comm_rank, comm_size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+
+  int num_cols = comm_size;
+  int num_rows = 8;
+  float matrix[num_rows][num_cols];
+  float vector[num_cols];
+  float true_result[num_rows];
+
+  // Allocate initial matrix and vector.
+  if (comm_rank == 0) {
+
+    for (int col_idx = 0; col_idx < num_cols; ++col_idx) {
+      vector[col_idx] = col_idx + 1;
+      for (int row_idx = 0; row_idx < num_rows; ++row_idx) {
+        matrix[row_idx][col_idx] = (col_idx+1)*(row_idx+1);
+      }
+    }
+    serial_product(num_rows, num_cols, matrix, vector, true_result);
+    print_vector(num_cols, vector);
+    print_matrix(num_rows, num_cols, matrix);
+    print_vector(num_rows, true_result);
+  }
+
+  // Scatter matrix
+  int local_num_rows = num_rows / comm_size;
+  float local_matrix[local_num_rows][num_cols];
+  int local_num_matrix_elements = local_num_rows * num_cols;
+  MPI_Scatter(matrix, local_num_matrix_elements, MPI_FLOAT, 
+              local_matrix, local_num_matrix_elements, MPI_FLOAT,
+              0, MPI_COMM_WORLD);
+
+  // Scatter vector
+  int local_vector_size = num_cols / comm_size;
+  float local_vector[local_vector_size];
+  MPI_Scatter(vector, local_vector_size, MPI_FLOAT,
+              local_vector, local_vector_size, MPI_FLOAT,
+              0, MPI_COMM_WORLD);
+
+  // Gather vector onto each process
+  MPI_Allgather(local_vector, local_vector_size, MPI_FLOAT,
+                vector, num_cols, MPI_FLOAT,
+                MPI_COMM_WORLD);
+
+  // Compute local result
+  float local_result[local_num_rows];
+  serial_product(local_num_rows, num_cols, local_matrix, vector, local_result);
+
+  // Gather final result
+  float result[num_rows];
+  MPI_Gather(local_result, local_num_rows, MPI_FLOAT,
+             result, local_num_rows, MPI_FLOAT,
+             0, MPI_COMM_WORLD);
+
+  if (comm_rank == 0) print_vector(num_rows, result);
+
+  MPI_Finalize();
+  return 0;
+}
